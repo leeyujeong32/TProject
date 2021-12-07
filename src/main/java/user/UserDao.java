be7e1101c7c4a9ee42e203a406d36ed995dfcc7b
이세은 <@DESKTOP-9JCSFBH>
2021-12-07 16:51:10 +0900

    sst deleted

--- src/main/java/user/UserDao.java
+++ src/main/java/user/UserDao.java
@@ -1,11 +1,10 @@
 package user;
 
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDao {
 	
-	SqlSessionTemplate sst;
+	
 
 }
