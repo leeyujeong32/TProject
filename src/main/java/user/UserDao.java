c22e337a2ac130bd01f6cad58e88ab7f85081fd8
이세은 <@DESKTOP-9JCSFBH>
2021-12-08 14:40:14 +0900

    sst update

--- src/main/java/user/UserDao.java
+++ src/main/java/user/UserDao.java
@@ -3,15 +3,16 @@ package user;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserDao {
-	
+	@Autowired
 	SqlSessionTemplate sst;
 
 	public List<UserVo> selectList(UserVo vo) {
-		return sst.selectList("user.selectList", vo);
+		return sst.selectList("theproject.selectList", vo);
 	}
 
 }
