9950a8231de12c5ad07e5fbf538581d22831071d
kgh <tmzlqhdl@gmail.com>
2022-01-05 17:51:45 +0900

    충돌확인

--- src/main/java/user/UserController.java
+++ src/main/java/user/UserController.java
@@ -3,11 +3,8 @@ package user;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -26,6 +23,8 @@ public class UserController {
 		model.addAttribute("list", list);
 		return "user/index";
 	}
+
+	
 	
 }
 
